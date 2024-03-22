b540d0454980e3bdc71e8c77d68db2bfde843a58
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-03-22 10:35:37 -0700

    lint

--- spec/example_app/db/schema.rb
+++ spec/example_app/db/schema.rb
@@ -21,8 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_181655) do
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["record_type", "record_id", "name"],
-      name: "index_action_text_rich_texts_uniqueness", unique: true
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -32,8 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_181655) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"],
-      name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -51,8 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_181655) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"],
-      name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "blog_posts", id: :serial, force: :cascade do |t|
@@ -172,10 +169,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_181655) do
     t.string "name", null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs",
-    column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs",
-    column: "blob_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "orders", "customers"
